28bcee2ff19f727ef4d3fa1b148f08bd831a2b62
tkashdat <tiffanykashima@gmail.com>
2025-08-20 21:27:59 -0700

    added unti tests and data import funtion

--- src/data_loading_functions.py
+++ src/data_loading_functions.py
@@ -1,4 +1,5 @@
-#This function loads the dicom image data for one study containing an image stack for one individual
+#This function loads the dicom image data for one study containing an image stack for one individualimpo
+import imageio as iio
 
 def loadStudy():
     filename = input("Enter path to study folder: ")
--- test/test_functions.py
+++ test/test_functions.py
@@ -1,14 +1,16 @@
 # unit tests for functions in src/functions.py
+import data_loading_functions as dlf
+import unittest
 
 class TestFileLoading(unittest.TestCase):
     def test_file_keys(self):
-        loadStudy()
+        dlf.loadStudy()
         self.assertTrue('SAG 3D T1 SPGR','20100204',(256,256))
     
     def test_file_type(self):
-        loadStudy()
+        dlf.loadStudy()
         self.assertRaises(TypeError)
 
     def test_file_size(self):
-        loadStudy()
-        self.assertTrue()
+        dlf.loadStudy()
+        self.assertTrue('SAG 3D T1 SPGR','20100204',(256,256))
