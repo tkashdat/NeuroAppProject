ca019db31dcbbe56060b68507726c78c7bd01836
tkashdat <tiffanykashima@gmail.com>
2025-08-27 09:38:20 -0700

    refactored app functions

--- src/app_functions.py
+++ src/app_functions.py
@@ -2,8 +2,9 @@
 import matplotlib.pyplot as plt
 
 def showSlice(fileName,sliceNum):
-    plt.imshow(fileName[sliceNum,:,:], smap='gray')
+    plt.imshow(fileName[sliceNum,:,:], cmap='gray')
     plt.axis('off')
+    plt.title(f"This is slice number {sliceNum} in axial view in gray tone")
     plt.shw()
 
 def metaData(fileName):
@@ -11,21 +12,16 @@ def metaData(fileName):
 
 def numSlices(fileName):
     x,y,z = fileName.shape
-    print("Number of slices per view:nt", "Axial: ",x," nt","Coronal: ", y, " nt", "Saggital: ", z, " nt")
+    print(f"Number of slices per view: Axial: {x}, Coronal: {y}, Transverse: {z}")
 
 def pickSliceAxis(fileName):
 
     n0, n1, n2 = fileName.shape
 
-    try:
-        sliceNum = input("Specifiy the slice number you would like to view: ")
-    except TypeError:
-        print("Slice number is not an integer")
+    sliceNum = input("Specifiy the slice number you would like to view: ")
+
+    axis = input("Specify which axis you would like to view (axial, coronal, transverse): ")
     
-    try:
-        axis = input("Specify which axis you would like to view (axial, coronal, transverse): ")
-    except:
-        print("Not a valid axis selection, please try again.")
     
     if axis == "axial" and sliceNum <= n0:
         plt.imshow(fileName[sliceNum,:,:], cmap='gray')
@@ -42,6 +38,8 @@ def pickSliceAxis(fileName):
         plt.axis('off')
         plt.shw()
 
+    else:
+        print("Invalid entry")
 
 def sliceColor(promptMessage):
     colors = ('gray', 'jet', 'bone')
