1a1db291815647c8342b1280b456767854238fad
tkashdat <tiffanykashima@gmail.com>
2025-08-28 12:41:15 -0700

    added comments to functions

--- src/app_functions.py
+++ src/app_functions.py
@@ -1,8 +1,12 @@
-#custom functions for use across the project
+#custom functions for use across the project in notebook
 import matplotlib.pyplot as plt
 import scipy.ndimage as ndi
 
 def showSlice(fileName):
+    """
+     Takes user input (integer value) for a single slice in the saggital plane to display (default cmap).  
+     Input must be an integer in range of the available slices in the saggital plane for specified study.
+    """
     while True: 
         printSlices(fileName)
         sliceNum = int(input("What number slice would you like in saggital view?"))
@@ -18,20 +22,40 @@ def showSlice(fileName):
             continue
 
 def metaData(fileName):
+    """
+    Takes a processed DICOM file (from loadVolume() method).
+    """
     return(fileName.meta.keys())
 
 def printSlices(fileName):
+    """
+    Takes a processed DICOM file (from loadVolume() method). 
+    Uses the shape method to store the number of slices (integer) in the study by plane 
+    (saggital, coronal, axial) into three variables, respectively, and returns a print statement.
+    """
     x,y,z = fileName.shape
     return(f"Number of slices per view: Saggital: {x}, Axial: {y}, Coronal: {z}")
 
 def numSlices(fileName):
+    """
+    Takes a processed DICOM file (from loadVolume() method). 
+    Uses the shape method to store the number of slices (integer) in the study by plane 
+    (saggital, coronal, axial) into three variables, respectively.
+    """
     x,y,z = fileName.shape
     return(x,y,z)
 
 def pickSliceAxis(fileName):
+    """
+        Takes a processed DICOM file (from loadVolume() method).  
+        Takes user input (axis: string value) for one specific plane (axis): saggital, coronal, or axial.  
+        Takes additional user input (sliceNum: integer value) for the number slice to view.  
+        Value must be less than or equal to the number of slices (n0, n1, n2).  
+        Uses matplotlib to create a slice graphic based on the user input in the 'bone' color map.
+    """
     n0, n1, n2 = fileName.shape
     #printSlices(fileName)
-    axis = input("Specify which axis you would like to view (saggital, axial, coronal: ").lower()
+    axis = input("Specify which axis you would like to view (saggital, axial, coronal: ").lower() #prevent capitalization issues
     sliceNum = int(input("Specifiy the slice number you would like to view: "))
     
     if axis == "saggital" and sliceNum <= n0:
@@ -41,13 +65,13 @@ def pickSliceAxis(fileName):
         plt.show()
 
     elif axis == "axial" and sliceNum <= n1:
-        plt.imshow(ndi.rotate(fileName[:,sliceNum,:],270), cmap='bone')
+        plt.imshow(ndi.rotate(fileName[:,sliceNum,:],270), cmap='bone') #rotates image 270 degrees for standard orientation
         plt.axis('off')
         plt.title(f"This is slice number {sliceNum} in {axis} view in bone color map")
         plt.show()
 
     elif axis == "coronal" and sliceNum <= n2:
-        plt.imshow(ndi.rotate(fileName[:,:,sliceNum],270), cmap='bone')
+        plt.imshow(ndi.rotate(fileName[:,:,sliceNum],270), cmap='bone') #rotates image 270 degrees for standard orientation
         plt.axis('off')
         plt.title(f"This is slice number {sliceNum} in {axis} view in bone color map")
         plt.show()
@@ -55,7 +79,15 @@ def pickSliceAxis(fileName):
     else:
         print("Invalid entry")
 
-def completeSlice(fileName, color):
+def completeSlice(fileName, color): #color argument can use sliceColor method
+    """
+        Takes a processed DICOM file (from loadVolume() method).  
+        Takes user input (sliceNum: string value) for one specific plane (axis): saggital, coronal, or axial.  
+        Takes user input (axis: integer value) for the number slice to view.  
+        Takes user input (color argument) for cmap value.
+        Value must be less than or equal to the number of slices (n0, n1, n2).  
+        Uses matplotlib to create a slice graphic based on the user input in the specified color map.
+    """
     n0, n1, n2 = fileName.shape
     #printSlices(fileName)
     axis = input("Specify which axis you would like to view (saggital, axial, coronal): ").lower()
@@ -69,13 +101,13 @@ def completeSlice(fileName, color):
         plt.show()
 
     elif axis == "axial" and sliceNum <= n1:
-        plt.imshow(ndi.rotate(fileName[:,sliceNum,:],270), cmap=color)
+        plt.imshow(ndi.rotate(fileName[:,sliceNum,:],270), cmap=color) #rotates image 270 degrees for standard orientation
         plt.title(f"This is slice number {sliceNum} in {axis} view in color map {color}")
         plt.axis('off')
         plt.show()
 
     elif axis == "coronal" and sliceNum <= n2:
-        plt.imshow(ndi.rotate(fileName[:,:,sliceNum],270), cmap=color)
+        plt.imshow(ndi.rotate(fileName[:,:,sliceNum],270), cmap=color) #rotates image 270 degrees for standard orientation
         plt.title(f"This is slice number {sliceNum} in {axis} view in color map {color}")
         plt.axis('off')
         plt.show()
@@ -84,14 +116,26 @@ def completeSlice(fileName, color):
         print("Invalid entry")
 
 def sliceColor():
+    """
+        Prompts the user to input the name of a color map to use in matplotlib methods in generating 
+        MRI image figures.  Must be 'jet' (color), 'bone' (darker greyscale), or 'gray' (light grayscale).  
+        Input must be a string value.
+    """
     colors = ('gray', 'jet', 'bone')
-    user_input = input("Choose color: gray, jet, or bone").lower()
+    user_input = input("Choose color: gray, jet, or bone").lower() #prevent issues with capitalization
     if user_input in colors:
         return(user_input)
     else:
         print("That is not a valid entry")
 
 def getAspectRatio(fileName):
+    """
+    Takes a processed DICOM file (from loadVolume() method). 
+    Stores the float values from the 'sampling' key from the metadata dictionary for fileName into 
+    three variables(d0, d1, d2), respectively.  These values represent physical distances used in the MRI scan process.  
+    These values can be used to re-create the correct aspect ratios in each plane (saggital, coronal, axial) 
+    for viewing slices.
+    """
     d0, d1, d2 = fileName.meta['sampling']
     axial_asp = d1/d2
     saggital_asp = d0/d1
--- src/data_loading_functions.py
+++ src/data_loading_functions.py
@@ -1,7 +1,11 @@
-#This function loads the dicom image data for one study containing an image stack for one individualimpo
+#This function loads the dicom image data for one study containing an image stack for one individual
 import imageio as iio
 
 def loadVolume():
+    """
+    Prompts user to input the path to the location of the study folder. 
+    Must be a folder containing DICOM MRI images.
+    """
     filename = input("Enter path to study folder: ")
     vol = iio.volread(filename, 'DICOM')
     print(f"Description: {vol.meta['SeriesDescription']}, Date: {vol.meta['SeriesDate']}, Size: {vol.shape}")
